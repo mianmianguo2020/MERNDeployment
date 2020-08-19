a2ede302e68477664e7edc9c2158212cb4f713f2
Christy <mianmianguo2020@gmail.com>
2020-08-18 20:58:23 -0400

    int

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,18 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
+import Sidebar from "./Sidebar";
 
 function App() {
   return (
-    <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Welcome to React</h2>
-      </div>
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
+    <div className="app">
+     <div className="app_body">
+     <Sidebar/>
+       </div>   
+       
     </div>
   );
 }
 
 
 export default App;
+
