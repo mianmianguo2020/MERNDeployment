52dfa03c426f2d52eb0f59fdff57e96339044cff
Christy <mianmianguo2020@gmail.com>
2020-08-26 16:15:53 -0400

    restore working react app

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,12 @@
 import React from "react";
 import "./App.css";
-import Sidebar from "./Sidebar";
-import Chat from "./Chat";
+
 
 function App() {
   return (
     <div className="app">
      <div className="app_body">
-     <Sidebar/>
-     <Chat />
+
        </div>   
        
     </div>
